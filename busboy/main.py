7fd5a1e412f7b6c6742305395d350dc1a297c54b
Noel Bourke <undergroundquizscene@gmail.com>
2018-12-29 22:56:23 +0000

    Fill database sequentially per thread

--- busboy/main.py
+++ busboy/main.py
@@ -35,32 +35,20 @@ def cycle(stops: Iterable[str], frequency: float, pool: Executor, terminate: Eve
     if not terminate.is_set():
         print(f"Cycling at {strftime('%X')}")
         Timer(frequency, cycle, args=[stops, frequency, pool, terminate]).start()
-        futures = [pool.submit(make_requests, StopId(stop), pool) for stop in stops]
-        for f in as_completed(futures):
+        futures = [pool.submit(make_requests, StopId(stop)) for stop in stops]
+        for i, f in enumerate(as_completed(futures), start=1):
             results = f.result()
-            errors = [r for r in results if r is not None]
-            integrity_errors = [e for e in errors if isinstance(e, pp2.IntegrityError)]
-            refused_errors = [
-                e for e in errors if isinstance(e, pp2.OperationalError)
-                and e.args[0].startswith("could not connect to server: Connection refused")
-            ]
-            print(f"Got {len(results)} results, {len(errors)} errors, {len(integrity_errors)} integrity errors, and {len(refused_errors)} refused errors")
-            eset = {repr(e) for e in errors if not isinstance(e, pp2.IntegrityError)}
-            print(f"Got eset: {eset}")
-            for e in errors:
-                if isinstance(e, pp2.IntegrityError):
-                    pass
-                elif isinstance(e, pp2.OperationalError) and e.args[0].startswith("could not connect to server: Connection refused"):
-                    pass
-                else:
-                    # print(f"Got unknown error {repr(e)}")
-                    pass
+            errors = (r for r in results if r is not None)
+            bad_errors = [e for e in errors if not isinstance(e, pp2.IntegrityError)]
+            error_types = {repr(e) for e in bad_errors}
+            print(f"Stop {i}: Got {len(results)} results, {len(bad_errors)} errors, error types: {error_types}")
 
-
-def make_requests(stop: StopId, pool: Executor) -> List[Optional[Exception]]:
+def make_requests(stop: StopId) -> List[Optional[Exception]]:
     spr = stop_passage(stop)
-    fs = [pool.submit(db.store_trip, p) for p in spr.passages]
-    return [f.result() for f in as_completed(fs)]
+    c = db.default_connection()
+    errors = [db.store_trip(p, connection=c) for p in spr.passages]
+    c.close()
+    return errors
 
 
 def make_request(url, trip):
