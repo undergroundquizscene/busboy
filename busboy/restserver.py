7df8c49e1e1f7b2ce5adc7ff18fd5be0eab5c309
Noel Bourke <undergroundquizscene@gmail.com>
2019-01-09 00:52:24 +0000

    Add test route for getting trip points

--- busboy/restserver.py
+++ busboy/restserver.py
@@ -7,5 +7,9 @@ import busboy.constants as c
 def hello():
    return f"<h1>Hello Down There!</h1><p>Church cross east’s id: {c.church_cross_east}</p>"
 
+@app.route("/trip/<trip_id>/")
+def trip_points(trip_id):
+    return f"<h1>You asked for {trip_id}!</h1>"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
