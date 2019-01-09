4938dfec1b97dc879e626615142e2ad0099f65b3
Noel Bourke <undergroundquizscene@gmail.com>
2019-01-09 15:51:49 +0000

    Add rest endpoint for trips_on_day

--- busboy/restserver.py
+++ busboy/restserver.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, send_file
+import datetime as dt
+from flask import Flask, send_file, abort
 app = Flask(__name__)
 
 import busboy.constants as c
@@ -15,6 +16,21 @@ def trip_points(trip_id: str):
     tps = db.trip_points(db.default_connection(), TripId(trip_id))
     response = json.dumps(tps.to_json())
     return (response, {"Content-Type": "text/json", "Access-Control-Allow-Origin": "*"})
+
+@app.route("/trips/<date>/")
+def trips_on_day(date: str):
+    try:
+        d = dt.date.fromisoformat(date)
+    except ValueError:
+        abort(400)
+    else:
+        trips = db.trips_on_day(
+            db.default_connection(),
+            d
+        )
+        body = [t.value for t in trips]
+        return (json.dumps(body), {"Content-Type": "text/json", "Access-Control-Allow-Origin": "*"})
+
 
 @app.route("/map.html")
 def map():
