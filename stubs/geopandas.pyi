c438b6dd3bafa3112057e82d18ed950a377306c6
Noel Bourke <undergroundquizscene@gmail.com>
2019-01-24 16:22:05 +0000

    Add basic geopandas stub

---
+++ stubs/geopandas.pyi
@@ -0,0 +1,10 @@
+from pandas import Series, DataFrame
+from typing import Dict, Any
+
+class GeoSeries(Series):
+    crs: Dict[str, str]
+    bounds: DataFrame
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None: ...
+    def to_crs(self, crs: Dict[str, str]) -> "GeoSeries": ...
+    def buffer(self, distance: float, resolution: float = 16, **kwargs: Any) -> "GeoSeries": ...
